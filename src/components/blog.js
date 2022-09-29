c37ed1272587bf6dcb9e9586dbd4d5ef709b6591
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-09-30 02:46:40 +0530

    file formatted

--- src/components/blog.js
+++ src/components/blog.js
@@ -4,14 +4,14 @@ import DataTable from "react-data-table-component";
 
 function Blog() {
   const [items, setItems] = useState([]);
-  const [search,setSearch] = useState("");
-  const [filtereddata,setFiltereddata] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filtereddata, setFiltereddata] = useState([]);
   const [pageCount, setpageCount] = useState(0);
 
   let limit = 20;
 
   useEffect(() => {
-    const getComments = async () => { 
+    const getComments = async () => {
       const res = await fetch(
         //`http://localhost:3004/comments?_page=1&_limit=${limit}`
         `https://jsonplaceholder.typicode.com/photos?_page=1&_limit=${limit}`
@@ -20,7 +20,7 @@ function Blog() {
       const total = res.headers.get("x-total-count");
       setpageCount(Math.ceil(total / limit));
       // console.log(Math.ceil(total/12));
-      setItems(data);      
+      setItems(data);
       setFiltereddata(data);
     };
 
@@ -30,7 +30,7 @@ function Blog() {
   const fetchComments = async (currentPage) => {
     const res = await fetch(
       //`http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
-       `https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=${limit}`
+      `https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=${limit}`
     );
     const data = await res.json();
     console.log(data);
@@ -50,28 +50,28 @@ function Blog() {
     //window.scrollTo(0, 0)
   };
 
-  useEffect(()=>{
-   const results = items.filter((field)=>{
+  useEffect(() => {
+    const results = items.filter((field) => {
       return field.title.toLowerCase().match(search.toLowerCase());
-   });
+    });
 
-   setFiltereddata(results);
-},[search]);
+    setFiltereddata(results);
+  }, [search]);
 
-  const columns= [
-   {
-     name: "ID",
-     selector: (row)=>row.id,
-   },
-   {
-     name: "Title",
-     selector: (row)=>row.title,
-   },
-   {
-     name: "Url",
-     selector: (row)=>row.url,
-   },  
- ]
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+    },
+    {
+      name: "Title",
+      selector: (row) => row.title,
+    },
+    {
+      name: "Url",
+      selector: (row) => row.url,
+    },
+  ];
   return (
     <div className="container">
       {/* <div className="row m-2">
@@ -93,21 +93,26 @@ function Blog() {
         })}
       </div> */}
 
-      <DataTable 
-	  title ="Employee Data"
-	  columns={columns} 
-	  data={filtereddata}     
-	// pagination 
-	  fixedHeader
-	  fixedHeaderScrollHeight="450px"
-	  selectableRows
-	  selectableRowsHighlight
-	  subHeader
-	  subHeaderComponent={
-		<input type="text" placeholder="Search here" className="w-25 form-control" value={search} onChange={(e)=>setSearch(e.target.value)} />
-	  }			  
-
-	/>
+      <DataTable
+        title="Employee Data"
+        columns={columns}
+        data={filtereddata}
+        // pagination
+        fixedHeader
+        fixedHeaderScrollHeight="450px"
+        selectableRows
+        selectableRowsHighlight
+        subHeader
+        subHeaderComponent={
+          <input
+            type="text"
+            placeholder="Search here"
+            className="w-25 form-control"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        }
+      />
 
       <ReactPaginate
         previousLabel={"previous"}
