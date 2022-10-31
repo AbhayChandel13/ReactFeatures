02378c25be611caa53efcbdfa39439a7e9dcb814
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-11-01 05:01:18 +0530

    code format

--- src/components/header.js
+++ src/components/header.js
@@ -1,39 +1,87 @@
-import React from 'react'
-import {NavLink}  from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Header(){
-	return(
-	<>
-	<nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-            <div className="container px-5">
-                <a className="navbar-brand" href="#page-top">START REACT </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item"><NavLink className="nav-link" to="/">Home</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to="/about">About Us</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to="/service">Service</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to="/blog">Blog</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to="/loadmore">Load More</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to="/scroll">Scroll</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to="/add">CRUD</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" >Log In</NavLink></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-	<header className="masthead text-center text-white">
-            <div className="masthead-content">
-                <div className="container px-5">
-                    <h1 className="masthead-heading mb-0">One Page Wonder</h1>
-                    <h2 className="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
-                    <a className="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
-                </div>
-            </div>
-    </header>
-	</>
-	);
-	
+function Header() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <div className="container px-5">
+          <a className="navbar-brand" href="#page-top">
+            START REACT{" "}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  About Us
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/service">
+                  Service
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/blog">
+                  Blog
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/loadmore">
+                  Load More
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/scroll">
+                  Scroll
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/add">
+                  CRUD
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link">Log In</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <header className="masthead text-center text-white">
+        <div className="masthead-content">
+          <div className="container px-5">
+            <h1 className="masthead-heading mb-0">One Page Wonder</h1>
+            <h2 className="masthead-subheading mb-0">
+              Will Rock Your Socks Off
+            </h2>
+            <a
+              className="btn btn-primary btn-xl rounded-pill mt-5"
+              href="#scroll"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
