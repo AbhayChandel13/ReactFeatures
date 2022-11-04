cded6dd187f0b770631b83045b8a5b1c0c34eefd
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-11-05 05:13:17 +0530

    code format

--- src/components/service.js
+++ src/components/service.js
@@ -1,99 +1,97 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 
-function Service(){
-	const [getData,setData] = useState([]);
-	const [search,setSearch] = useState("");
-	const [filtereddata,setFiltereddata] = useState([]);
-	const getuser= async()=>{
-		try{
-			// https://jsonplaceholder.typicode.com/photos
-			// http://localhost:3000/getdata.php (// "proxy":"http://localhost:3000/getdata.php",)
-			// http://universities.hipolabs.com/search?country=United+States
-		const res = await fetch("https://jsonplaceholder.typicode.com/photos");
-	    const data = await res.json();
-		 setData(data);
-		 setFiltereddata(data);
-		 //console.log("data",data);
-		 }
-		catch(err){
-			console.log(err);
-		}
-	};
-	
-	useEffect(()=>{
-		getuser();
-	},[]);
+function Service() {
+  const [getData, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filtereddata, setFiltereddata] = useState([]);
+  const getuser = async () => {
+    try {
+      // https://jsonplaceholder.typicode.com/photos
+      // http://localhost:3000/getdata.php (// "proxy":"http://localhost:3000/getdata.php",)
+      // http://universities.hipolabs.com/search?country=United+States
+      const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+      const data = await res.json();
+      setData(data);
+      setFiltereddata(data);
+      //console.log("data",data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	useEffect(()=>{
-		const results = getData.filter((field)=>{
-			return field.title.toLowerCase().match(search.toLowerCase());
-		});
+  useEffect(() => {
+    getuser();
+  }, []);
 
-		setFiltereddata(results);
-	},[search]);
+  useEffect(() => {
+    const results = getData.filter((field) => {
+      return field.title.toLowerCase().match(search.toLowerCase());
+    });
 
-	const columns= [
-      {
-        name: "ID",
-        selector: (row)=>row.id,
-      },
-      {
-        name: "Title",
-        selector: (row)=>row.title,
-      },
-      {
-        name: "Url",
-        selector: (row)=>row.url,
-      },  
-    ]
+    setFiltereddata(results);
+  }, [search]);
 
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+    },
+    {
+      name: "Title",
+      selector: (row) => row.title,
+    },
+    {
+      name: "Url",
+      selector: (row) => row.url,
+    },
+  ];
 
-	const loadScript = (src) => {
-		return new Promise((resolve) => {
-		  const script = document.createElement("script");
-		  script.src = src;
-		  script.onload = () => {
-			resolve(true);
-		  };
-		  script.onerror = () => {
-			resolve(false);
-		  };
-		 document.body.appendChild(script);
-	   });
-	};
-	
-	useEffect(() => {
-		loadScript("https://checkout.razorpay.com/v1/checkout.js");
-	});
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  };
 
-	
-	// const paymentObject = new window.Razorpay();
-    //  paymentObject.open();
+  useEffect(() => {
+    loadScript("https://checkout.razorpay.com/v1/checkout.js");
+  });
 
-return(
- <>
-    <h1>Hi From the Service Page  </h1>
-	<DataTable 
-	  title ="Employee Data"
-	  columns={columns} 
-	  data={filtereddata}
-	  pagination 
-	  fixedHeader
-	  fixedHeaderScrollHeight="450px"
-	  selectableRows
-	  selectableRowsHighlight
-	  subHeader
-	  subHeaderComponent={
-		<input type="text" placeholder="Search here" className="w-25 form-control" value={search} onChange={(e)=>setSearch(e.target.value)} />
-	  }			  
+  // const paymentObject = new window.Razorpay();
+  //  paymentObject.open();
 
-	/>
-
-
-	  
- </>
-
-);
+  return (
+    <>
+      <h1>Hi From the Service Page </h1>
+      <DataTable
+        title="Employee Data"
+        columns={columns}
+        data={filtereddata}
+        pagination
+        fixedHeader
+        fixedHeaderScrollHeight="450px"
+        selectableRows
+        selectableRowsHighlight
+        subHeader
+        subHeaderComponent={
+          <input
+            type="text"
+            placeholder="Search here"
+            className="w-25 form-control"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        }
+      />
+    </>
+  );
 }
-export default Service ;
+export default Service;
